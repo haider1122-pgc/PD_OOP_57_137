eef82572eb654520e3d8a29f92fe6de4d6797fa7
MZT-Venom <81874355+MZT-Venom@users.noreply.github.com>
2021-04-24 20:16:50 +0500

    updated Task 3
    Encryption method Added

--- Task3/src/task3/Task3.java
+++ Task3/src/task3/Task3.java
@@ -5,7 +5,10 @@
  */
 package task3;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 
 /**
  *
@@ -16,10 +19,11 @@ public class Task3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException {
+        Cipher c = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding") ;
         // TODO code application logic here
         Scanner s = new Scanner(System.in);
-         String text,text1;
+         String text,text1,text2;
          System.out.println(">Removing Unusuall Spaces");
          System.out.println("Input String: ");
          text=s.nextLine();
@@ -31,6 +35,11 @@ public class Task3 {
          text1=s.nextLine();
          System.out.println("");
          System.out.println(removeInteger(text1));
+         System.out.println(">Encrypting the String");
+         System.out.println("Input String: ");
+         text2=s.nextLine();
+         System.out.print(stringEncryption(text2));
+         
     }
      /**
     * 
@@ -53,4 +62,27 @@ public class Task3 {
        k = s.replaceAll("[0-9]","");
        return k;
    }
+   /**
+    * 
+    * this function will encrypt the given string using basic encryption scheme
+    * @param s
+    * @return 
+    */
+   public static String stringEncryption(String s){
+       
+   char[] chars=s.toCharArray();
+   for(int i=0;i<s.length();i++){
+   if(i%2!=0){
+   chars[i]=(char) (s.charAt(i)+9);
+   }
+   else if(i%3!=0){
+   chars[i]=(char) (s.charAt(i)-4);
+   }
+   else{
+   chars[i]=(char) (s.charAt(i)+12);
+   }
+   }
+   return chars.toString();
+   }
+   
 }

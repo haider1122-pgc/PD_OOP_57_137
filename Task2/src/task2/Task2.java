902fef47a46ef74257aa90910366a9b2227ad141
haider1122-pgc <70263008+haider1122-pgc@users.noreply.github.com>
2021-04-24 15:51:46 -0700

    task2 complete

---
+++ Task2/src/task2/Task2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Haider
+ */
+public class Task2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner s = new Scanner(System.in);
+         String text;
+         System.out.println("Please Enter a Sentence:");
+         text=s.nextLine();
+         System.out.println("Your Sentence Printed Vertically is:");
+        displayVertical(text);
+    }
+    /**
+     * task 2 function
+     * this function is used to print a string vertically
+     * @param s 
+     */
+   public static void displayVertical(String s){
+       int size=s.length();
+       char[] str=new char[size];
+       int j=0;
+       for(int i=0;i<size;i++){
+           if(s.charAt(i)!= ' '){
+            str[j]=s.charAt(i);
+            j++;
+           }
+       }
+       
+       for(int i=0;i<size;i++){
+           System.out.println(str[i]);
+       }
+   }
+}
